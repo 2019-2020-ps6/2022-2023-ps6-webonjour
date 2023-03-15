f8eddd9930a353e0fc2da2b01fcb445bb7b47b1e
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-03-15 10:47:22 +0100

    feat: add loading from localstorage on init
    ref: #21

--- libs/util-interface/src/index.ts
+++ libs/util-interface/src/index.ts
@@ -1,1 +1,12 @@
 export * as Auth from './lib/auth/index';
+
+enum RequestStatus {
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+
+export interface RequestWrapper<RequestData> {
+  data: RequestData;
+  status: RequestStatus;
+  message: string;
+}
