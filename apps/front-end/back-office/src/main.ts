cab737cb3a4a23cf6822974fd9abbcb2b5336ffe
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-03-15 19:31:48 +0100

    refactor: migrate to modules
    ref: #35

--- apps/front-end/back-office/src/main.ts
+++ apps/front-end/back-office/src/main.ts
@@ -1,16 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/app.routes';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    importProvidersFrom(BrowserAnimationsModule),
-  ],
-}).catch((err) => console.error(err));
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
