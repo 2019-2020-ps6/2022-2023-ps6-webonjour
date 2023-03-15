0340b6f9ebdc9ba1a5985554890dc66d0af03640
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-03-15 19:32:35 +0100

    feat: generate login page
    ref: #35

--- apps/front-end/back-office/src/app/app.routes.ts
+++ apps/front-end/back-office/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
