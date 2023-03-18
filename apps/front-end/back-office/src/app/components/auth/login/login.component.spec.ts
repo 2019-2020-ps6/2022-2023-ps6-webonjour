49540ecdffeeb19e0d29b62cdd6a8c5dcabcb426
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-03-18 16:31:22 +0100

    refactor: remove unnecessary imports
    ref: #35

--- apps/front-end/back-office/src/app/components/auth/login/login.component.spec.ts
+++ apps/front-end/back-office/src/app/components/auth/login/login.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { LoginFormComponent } from '../login-form/login-form.component';
 import { Component } from '@angular/core';
 
 describe('LoginComponent', () => {
