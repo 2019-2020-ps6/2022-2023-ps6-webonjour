686391442a2daab553415ff775fabe2d264b0535
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-03-18 16:38:45 +0100

    refactor: reduce method complexity
    ref: #33

--- apps/front-end/back-office/src/app/interceptors/jwt.interceptor.ts
+++ apps/front-end/back-office/src/app/interceptors/jwt.interceptor.ts
@@ -27,33 +27,40 @@ export class JwtInterceptor implements HttpInterceptor {
       catchError((err) => {
         if (err instanceof HttpErrorResponse) {
           if (err.status === 401) {
-            return this.authService.refresh().pipe(
-              switchMap(() => {
-                request = request.clone({
-                  setHeaders: {
-                    Authorization: `Bearer ${this.authService.accessToken}`,
-                  },
-                });
-                return next.handle(request).pipe(
-                  catchError((err) => {
-                    if (err instanceof HttpErrorResponse) {
-                      if (err.status === 401) {
-                        this.router.navigate(['/login']);
-                      }
-                    }
-                    return throwError(() => err);
-                  })
-                );
-              }),
-              catchError((err) => {
-                if (err instanceof HttpErrorResponse) {
-                  if (err.status === 401) {
-                    this.router.navigate(['/login']);
-                  }
-                }
-                return throwError(() => err);
-              })
-            );
+            return this.handleUnauthorized(request, next);
+          }
+        }
+        return throwError(() => err);
+      })
+    );
+  }
+
+  private handleUnauthorized(request: HttpRequest<unknown>, next: HttpHandler) {
+    // refresh access token with refresh token
+    return this.authService.refresh().pipe(
+      switchMap(() => {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${this.authService.accessToken}`,
+          },
+        });
+        return next.handle(request).pipe(
+          catchError((err) => {
+            if (err instanceof HttpErrorResponse) {
+              if (err.status === 401) {
+                // if the access token is still expired, redirect to login page
+                this.router.navigate(['/login']).then();
+              }
+            }
+            return throwError(() => err);
+          })
+        );
+      }),
+      catchError((err) => {
+        // if refresh token is expired, redirect to login page
+        if (err instanceof HttpErrorResponse) {
+          if (err.status === 401) {
+            this.router.navigate(['/login']).then();
           }
         }
         return throwError(() => err);
