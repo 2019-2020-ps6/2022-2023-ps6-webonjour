e26fd0a2e91fb928a386499a50613a78dac1bb51
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-03-19 15:48:41 +0100

    fix: auth tests
    ref: #43

--- libs/data-access-fake-backend/src/lib/mocks/auth.ts
+++ libs/data-access-fake-backend/src/lib/mocks/auth.ts
@@ -10,3 +10,21 @@ export const response: Auth.LoginResponse = {
   refreshToken:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1pY2hlbGxlIERvZSIsImlhdCI6MTUxNjIzOTAyMiwidHlwZSI6InJlZnJlc2giLCJwcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vY2RuLWFqZ2dkLm5pdHJvY2RuLmNvbS9rTW9PRnBEbHNPVnRsWUpMcm5TUk5DUVhhVUZIWlBUWS9hc3NldHMvaW1hZ2VzL29wdGltaXplZC9yZXYtNDRiMDAyYi93cC1jb250ZW50L3VwbG9hZHMvYmItcGx1Z2luL2NhY2hlL2Nvb2wtcHJvZmlsZS1waWMtbWF0aGV1cy1mZXJyZXJvLWxhbmRzY2FwZS5qcGVnIn0.VXJhFCxx3f5piStCo44a6MDIIfywmLc2Z4mgAtR1Stc',
 };
+
+export const decodedAccessToken = {
+  sub: '1234567890',
+  name: 'Michelle Doe',
+  iat: 1516239022,
+  type: 'access',
+  profilePicture:
+    'https://cdn-ajggd.nitrocdn.com/kMoOFpDlsOVtlYJLrnSRNCQXaUFHZPTY/assets/images/optimized/rev-44b002b/wp-content/uploads/bb-plugin/cache/cool-profile-pic-matheus-ferrero-landscape.jpeg',
+};
+
+export const decodedRefreshToken = {
+  sub: '1234567890',
+  name: 'Michelle Doe',
+  iat: 1516239022,
+  type: 'refresh',
+  profilePicture:
+    'https://cdn-ajggd.nitrocdn.com/kMoOFpDlsOVtlYJLrnSRNCQXaUFHZPTY/assets/images/optimized/rev-44b002b/wp-content/uploads/bb-plugin/cache/cool-profile-pic-matheus-ferrero-landscape.jpeg',
+};
