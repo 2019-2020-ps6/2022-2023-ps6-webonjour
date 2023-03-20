ffa3d736fbefdac7be426b33a6d0bcc071da1c1b
Maxime BILLY <maxime.billy@etu.univ-cotedazur.fr>
2023-03-20 10:33:47 +0100

    format: fix formatting
    Ref. #47

--- apps/front-end/front-office/src/app/components/quiz/quiz-item/quiz-item.component.ts
+++ apps/front-end/front-office/src/app/components/quiz/quiz-item/quiz-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Quiz } from '@webonjour/util-interface';
 
 @Component({
@@ -7,7 +7,6 @@ import { Quiz } from '@webonjour/util-interface';
   styleUrls: ['./quiz-item.component.scss'],
 })
 export class QuizItemComponent {
-
   @Input() quiz!: Quiz.Quiz;
 
   get quizTitle(): string {
@@ -26,8 +25,9 @@ export class QuizItemComponent {
     this.quiz = {
       id: '1',
       title: 'Les Chevaux Célèbres',
-      imageUrl: 'https://images.unsplash.com/photo-1589985851119-8e1f2e1b2e1c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hldmF4fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80',
+      imageUrl:
+        'https://images.unsplash.com/photo-1589985851119-8e1f2e1b2e1c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hldmF4fGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80',
       stage: 1,
-    }
+    };
   }
 }
