fd6b42a41ae4c82f2190cb7f9d8d4ae302e7c2ae
arnaud dumanois <arnaud.dumanois@etu.unice.fr>
2023-03-25 13:17:35 +0100

    feat: add test for component quiz-item
    ref #65

--- apps/front-end/front-office/src/app/components/quiz/quiz-item/quiz-item.component.spec.ts
+++ apps/front-end/front-office/src/app/components/quiz/quiz-item/quiz-item.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuizItemComponent } from './quiz-item.component';
 
+import { Quiz } from '@webonjour/util-interface';
+
 describe('QuizItemComponent', () => {
   let component: QuizItemComponent;
   let fixture: ComponentFixture<QuizItemComponent>;
@@ -18,5 +20,38 @@ describe('QuizItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should have a quiz input', () => {
+    expect(component.quiz).toBeDefined();
+  });
+
+  it('should have a quiz title', () => {
+    const quiz: Quiz.Quiz = {
+      id: '1',
+      title: 'Test Quiz',
+      imageUrl: '',
+      stage: 1
+    };
+    component.quiz = quiz;
+    expect(component.quizTitle).toEqual('Test Quiz');
+  });
+
+  it('should have a quiz image URL', () => {
+    const quiz: Quiz.Quiz = {
+      id: '1',
+      title: '',
+      imageUrl: 'http://example.com/image.jpg',
+      stage: 1
+    };
+    component.quiz = quiz;
+    expect(component.quizImageUrl).toEqual('http://example.com/image.jpg');
+  });
+
+  it('should call selectQuiz() method when quiz is clicked', () => {
+    spyOn(component, 'selectQuiz');
+    const quizElement = fixture.nativeElement.querySelector('.quiz-item');
+    quizElement.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    expect(component.selectQuiz).toHaveBeenCalled();
   });
 });
