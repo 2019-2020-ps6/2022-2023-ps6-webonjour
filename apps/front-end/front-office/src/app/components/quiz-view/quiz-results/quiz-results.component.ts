f6058261d62e71ba826b6c730df85177773e48ad
Maxime BILLY <maxime.billy@etu.univ-cotedazur.fr>
2023-03-27 13:56:31 +0200

    feat: add results tab
    Ref. #93

---
+++ apps/front-end/front-office/src/app/components/quiz-view/quiz-results/quiz-results.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'webonjour-quiz-results',
+  templateUrl: './quiz-results.component.html',
+  styleUrls: ['./quiz-results.component.scss'],
+})
+export class QuizResultsComponent {}
