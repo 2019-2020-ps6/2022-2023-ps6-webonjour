5f485286572a4c6a1fda33aeb0db7e029c4c2b76
Maxime BILLY <maxime.billy@etu.univ-cotedazur.fr>
2023-03-27 10:28:50 +0200

    feat: add hommepage
    Ref. #88

--- apps/front-end/front-office/src/app/app.routes.ts
+++ apps/front-end/front-office/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Route } from '@angular/router';
 import { QuizListItemComponent } from './components/quiz/quiz-list-item/quiz-list-item.component';
 import { GamePageComponent } from './components/quiz-view/game-page/game-page.component';
+import { CardContainerComponent } from './components/homepage/card-container/card-container.component';
 
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: CardContainerComponent,
+  },
   {
     path: 'list-quiz',
     component: QuizListItemComponent,
