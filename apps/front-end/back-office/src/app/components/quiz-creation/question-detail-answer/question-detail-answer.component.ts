244fb268410d27484c2812ee96a4cfa7b67a446a
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-03-27 14:04:14 +0200

    fix: another compile error

--- apps/front-end/back-office/src/app/components/quiz-creation/question-detail-answer/question-detail-answer.component.ts
+++ apps/front-end/back-office/src/app/components/quiz-creation/question-detail-answer/question-detail-answer.component.ts
@@ -4,7 +4,6 @@ import { Quiz } from '@webonjour/util-interface';
 @Component({
   selector: 'webonjour-question-detail-answer',
   templateUrl: './question-detail-answer.component.html',
-  styleUrls: ['./question-detail-answer.component.scss'],
 })
 export class QuestionDetailAnswerComponent {
   @Input() answer!: Quiz.Answer;
