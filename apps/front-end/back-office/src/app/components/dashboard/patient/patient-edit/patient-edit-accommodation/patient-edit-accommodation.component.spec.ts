939e43891f7a88060ae9fd106e3be2db7b25f252
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-01 17:34:44 +0200

    fix: tests
    Ref: #123

--- apps/front-end/back-office/src/app/components/dashboard/patient/patient-edit/patient-edit-accommodation/patient-edit-accommodation.component.spec.ts
+++ apps/front-end/back-office/src/app/components/dashboard/patient/patient-edit/patient-edit-accommodation/patient-edit-accommodation.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PatientEditAccommodationComponent } from './patient-edit-accommodation.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('PatientEditAccommodationComponent', () => {
   let component: PatientEditAccommodationComponent;
@@ -9,6 +14,13 @@ describe('PatientEditAccommodationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PatientEditAccommodationComponent],
+      imports: [
+        HttpClientTestingModule,
+        MatPaginatorModule,
+        MatTableModule,
+        RouterTestingModule,
+        MatDialogModule,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PatientEditAccommodationComponent);
