39c03dd4cbaef0efb940273b2125820b44150ce6
Maxime BILLY <maxime.billy@etu.univ-cotedazur.fr>
2023-04-01 22:44:35 +0200

    fix: hint should come from variable
    Ref. #128

--- apps/front-end/front-office/src/app/components/quiz-view/help-page/help-page-hint/help-page-hint.component.ts
+++ apps/front-end/front-office/src/app/components/quiz-view/help-page/help-page-hint/help-page-hint.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'webonjour-help-page-hint',
   templateUrl: './help-page-hint.component.html',
   styleUrls: ['./help-page-hint.component.scss'],
 })
-export class HelpPageHintComponent {}
+export class HelpPageHintComponent {
+  @Input() clue!: string;
+}
