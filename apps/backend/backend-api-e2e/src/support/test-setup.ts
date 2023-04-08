b077f385c3fe566818dd0f78173953730efb7983
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-08 18:28:19 +0200

    feat: connect express to mongoDB
    Ref: #152

--- apps/backend/backend-api-e2e/src/support/test-setup.ts
+++ apps/backend/backend-api-e2e/src/support/test-setup.ts
@@ -1,10 +1,36 @@
 /* eslint-disable */
-
 import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
+import { config } from 'dotenv';
 
 module.exports = async function () {
-  // Configure axios for tests to use.
-  const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ?? '3000';
+  // check if there is a .env file in the current directory
+  // if there is, load it else load the .env file in the api directory
+  if (fs.existsSync(path.resolve('.env'))) {
+    config({
+      path: path.resolve('.env'),
+      debug: true,
+    });
+  } else {
+    config({
+      path: path.resolve('apps/backend/backend-api-e2e/.env'),
+      debug: true,
+    });
+  }
+  let config2 = require('config');
+  const port = config2.get('port');
+  const host = config2.get('host');
   axios.defaults.baseURL = `http://${host}:${port}`;
+
+  // wait for the server to start
+  while (true) {
+    try {
+      await axios.get(`/health`);
+      break;
+    } catch (e) {
+      console.log('Waiting for server to start...');
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
 };
