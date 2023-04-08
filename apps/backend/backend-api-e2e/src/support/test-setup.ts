0799081bbf1c788856b035a7450d8903e9a37f0c
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-08 19:05:14 +0200

    fix: packages
    Ref: #152

--- apps/backend/backend-api-e2e/src/support/test-setup.ts
+++ apps/backend/backend-api-e2e/src/support/test-setup.ts
@@ -2,25 +2,26 @@
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
-import { config } from 'dotenv';
+import { config as dotenv } from 'dotenv';
+if (fs.existsSync(path.resolve('.env'))) {
+  dotenv({
+    path: path.resolve('.env'),
+    debug: true,
+  });
+} else {
+  dotenv({
+    path: path.resolve('apps/backend/backend-api-e2e/.env'),
+    debug: true,
+  });
+}
+import config from 'config';
 
 module.exports = async function () {
   // check if there is a .env file in the current directory
   // if there is, load it else load the .env file in the api directory
-  if (fs.existsSync(path.resolve('.env'))) {
-    config({
-      path: path.resolve('.env'),
-      debug: true,
-    });
-  } else {
-    config({
-      path: path.resolve('apps/backend/backend-api-e2e/.env'),
-      debug: true,
-    });
-  }
-  let config2 = require('config');
-  const port = config2.get('port');
-  const host = config2.get('host');
+
+  const port = config.get('port');
+  const host = config.get('host');
   axios.defaults.baseURL = `http://${host}:${port}`;
 
   // wait for the server to start
