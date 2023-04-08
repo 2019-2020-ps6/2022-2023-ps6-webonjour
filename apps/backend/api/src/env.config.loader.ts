676d9f9da9063dfb892b9277c8f80e21dbf08e57
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-08 18:28:19 +0200

    feat: connect express to mongoDB
    Ref: #152

---
+++ apps/backend/api/src/env.config.loader.ts
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as fs from 'fs';
+// check if there is a .env file in the current directory
+// if there is, load it else load the .env file in the api directory
+if (fs.existsSync(path.resolve('.env'))) {
+  config();
+} else {
+  config({ path: path.resolve('apps/backend/api/.env') });
+}
--- apps/backend/api/src/main.ts
+++ apps/backend/api/src/main.ts
@@ -1,7 +1,11 @@
+import './env.config.loader';
+import config from 'config';
 import express from 'express';
+import connectDB from './utils/connectDB';
+import mongoose from 'mongoose';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = config.get<string>('host');
+const port = config.get<number>('port');
 
 const app = express();
 
@@ -9,6 +13,18 @@ app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
+app.get('/health', (req, res) => {
+  // check if the database is connected
+  if (mongoose.connection.readyState !== 1) {
+    res.status(500).send({ message: 'Database not connected' });
+    return;
+  }
+  res.send({ message: 'OK' });
+});
+
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
+  connectDB().catch((error: Error) => {
+    console.log(error.message);
+  });
 });
