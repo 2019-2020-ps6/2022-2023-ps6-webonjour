b3a01f0a19cce5c99a785e1b6caa8e116c777e0f
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-08 20:11:10 +0200

    feat: add docker compose db
    Ref: #152

--- apps/backend/backend-api-e2e/src/support/global-teardown.ts
+++ apps/backend/backend-api-e2e/src/support/global-teardown.ts
@@ -1,12 +1,17 @@
 /* eslint-disable */
 
-import { killSubProcesses } from 'subspawn';
+import { killSubProcesses, subProcessSync } from 'subspawn';
 
 module.exports = async function () {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
   // stop the server
   killSubProcesses('API');
+  killSubProcesses('DB');
+  subProcessSync(
+    'docker-compose -f apps/backend/api/docker-compose.yml down',
+    true
+  );
 
   console.log(globalThis.__TEARDOWN_MESSAGE__);
 };
