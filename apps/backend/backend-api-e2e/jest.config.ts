c4b5a927896ada4a9ac515a6e782bbd25799d9e7
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-08 17:08:59 +0200

    fix: tests
    Ref: #149

--- apps/backend/backend-api-e2e/jest.config.ts
+++ apps/backend/backend-api-e2e/jest.config.ts
@@ -2,17 +2,12 @@
 export default {
   displayName: 'backend-backend-api-e2e',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
   setupFiles: ['<rootDir>/src/support/test-setup.ts'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../coverage/backend-backend-api-e2e',
--- apps/backend/backend-api-e2e/src/support/global-teardown.ts
+++ apps/backend/backend-api-e2e/src/support/global-teardown.ts
@@ -1,7 +1,12 @@
 /* eslint-disable */
 
+import { killSubProcesses } from 'subspawn';
+
 module.exports = async function () {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
+  // stop the server
+  killSubProcesses('API');
+
   console.log(globalThis.__TEARDOWN_MESSAGE__);
 };
