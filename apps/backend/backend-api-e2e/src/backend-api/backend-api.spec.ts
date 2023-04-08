af8fbc921ad43734593bb5ce49661aa7c1b5140d
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-04-08 20:17:12 +0200

    test: add e2e test for health
    Ref: #152

--- apps/backend/backend-api-e2e/src/backend-api/backend-api.spec.ts
+++ apps/backend/backend-api-e2e/src/backend-api/backend-api.spec.ts
@@ -8,3 +8,12 @@ describe('GET /', () => {
     expect(res.data).toEqual({ message: 'Hello API' });
   });
 });
+
+describe('GET /health', () => {
+  it('should return a message', async () => {
+    const res = await axios.get(`/health`);
+
+    expect(res.status).toBe(200);
+    expect(res.data).toEqual({ message: 'OK' });
+  });
+});
