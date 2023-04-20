3119d4548d247b6c8a27e3e49baa93ace3d44042
ArnaudDumanois <arnaud.dumanois@etu.univ-cotedazur.fr>
2023-04-20 18:50:29 +0200

    feat: add action and ngFro to the HTML    ref #171

--- apps/front-end/front-office/src/app/components/drag-and-drop/drag-and-drop.component.ts
+++ apps/front-end/front-office/src/app/components/drag-and-drop/drag-and-drop.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'webonjour-drag-and-drop',
   templateUrl: './drag-and-drop.component.html',
   styleUrls: ['./drag-and-drop.component.scss'],
 })
-export class DragAndDropComponent {}
+export class DragAndDropComponent {
+  actions = [
+    { name: 'action1', order: 1 },
+    { name: 'action2', order: 2 },
+    { name: 'action3', order: 3 },
+    { name: 'action4', order: 4 },
+  ];
+}
