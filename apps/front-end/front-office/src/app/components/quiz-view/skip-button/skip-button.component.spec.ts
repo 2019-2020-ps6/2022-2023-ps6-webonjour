652330bf5ed9f9e7b1fed2cb9c1aeff3aea1735d
ArnaudDumanois <arnaud.dumanois@etu.univ-cotedazur.fr>
2023-05-03 12:57:33 +0200

    fix: add providers on spec.ts ref #190

--- apps/front-end/front-office/src/app/components/quiz-view/skip-button/skip-button.component.spec.ts
+++ apps/front-end/front-office/src/app/components/quiz-view/skip-button/skip-button.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SkipButtonComponent } from './skip-button.component';
+import { StoreModule } from '@ngrx/store';
+import { fakeBackendProvider } from '@webonjour/data-access-fake-backend';
+import { provideMockStore } from '@ngrx/store/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SkipButtonComponent', () => {
   let component: SkipButtonComponent;
@@ -9,8 +12,12 @@ describe('SkipButtonComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SkipButtonComponent],
+      imports: [HttpClientTestingModule],
+      providers: [fakeBackendProvider, provideMockStore({})],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(SkipButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,4 +26,5 @@ describe('SkipButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  // test the Store
 });
