1c0456bcb817a82f7db55225ae3a9cbced24ce60
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-05-07 17:05:07 +0200

    test: fix missing provider for HttpClient
    Ref: #163

--- libs/front-end/shared/common/src/lib/services/tts/tts.service.spec.ts
+++ libs/front-end/shared/common/src/lib/services/tts/tts.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TtsService } from './tts.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TtsService', () => {
   let service: TtsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(TtsService);
   });
 
