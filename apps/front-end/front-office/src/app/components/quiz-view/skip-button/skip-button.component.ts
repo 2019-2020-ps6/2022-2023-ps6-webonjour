00e2a7991a475023b6b609ebde41e5cde841d0f8
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-05-07 16:08:08 +0200

    refactor: simplify SkipButtonComponent
    Ref: #190

--- apps/front-end/front-office/src/app/components/quiz-view/skip-button/skip-button.component.ts
+++ apps/front-end/front-office/src/app/components/quiz-view/skip-button/skip-button.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { selectPatientDiseaseStage } from '../../../reducers/game/game.selectors';
 import * as GameActions from '../../../reducers/game/game.actions';
-import { Quiz } from '@webonjour/util-interface';
-import { Input } from '@angular/core';
 
 @Component({
   selector: 'webonjour-skip-button',
@@ -12,17 +8,8 @@ import { Input } from '@angular/core';
   styleUrls: ['./skip-button.component.scss'],
 })
 export class SkipButtonComponent {
-  @Input() diseaseStage: Quiz.DiseaseStage = Quiz.DiseaseStage.STAGE_3;
-  @Input() answer: Quiz.Answer = { text: '', isCorrect: false };
-  constructor(private router: Router, private store: Store) {}
+  constructor(private store: Store) { }
 
-  ngOnInit() {
-    this.store.select(selectPatientDiseaseStage).subscribe((diseaseStage) => {
-      this.diseaseStage = diseaseStage
-        ? diseaseStage
-        : Quiz.DiseaseStage.STAGE_3;
-    });
-  }
   onClick() {
     this.store.dispatch(GameActions.nextQuestion());
   }
