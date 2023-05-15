cda6b9a5976f6992fa74779cb20029536bb23e1a
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-05-16 01:06:58 +0200

    fix: start order
    Ref: #246

--- apps/backend/backend-api-e2e/src/support/global-setup.ts
+++ apps/backend/backend-api-e2e/src/support/global-setup.ts
@@ -6,7 +6,6 @@ module.exports = async function () {
   // Start services that the app needs to run (e.g. database, docker-compose, etc.).
   console.log('\nSetting up...\n');
 
-  subProcess('API', 'npx nx serve backend-api', true);
   subProcessSync('docker compose -f docker-compose.yml up -d', true);
   // wait for the database to start
   for (let i = 0; i < 200; i++) {
@@ -19,6 +18,7 @@ module.exports = async function () {
       await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }
+  subProcess('API', 'npx nx serve backend-api', true);
 
   // Hint: Use `globalThis` to pass variables to global teardown.
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
