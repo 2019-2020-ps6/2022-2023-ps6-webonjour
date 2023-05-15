da133f5d8c6d423a57d80004cf5bbb7ef4cce72a
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-05-15 11:25:56 +0200

    fix: wait for db
    Ref: #246

--- apps/backend/backend-api-e2e/src/support/global-setup.ts
+++ apps/backend/backend-api-e2e/src/support/global-setup.ts
@@ -8,8 +8,17 @@ module.exports = async function () {
 
   subProcess('API', 'npx nx serve backend-api', true);
   subProcessSync('docker compose -f docker-compose.yml up -d', true);
-  subProcessSync('npx prisma migrate dev deploy', true);
-  subProcessSync('npx prisma generate', true);
+  // wait for the database to start
+  for (let i = 0; i < 200; i++) {
+    try {
+      subProcessSync('npx prisma migrate dev deploy', true);
+      subProcessSync('npx prisma generate', true);
+      break;
+    } catch (e) {
+      console.log('Waiting for database to start...');
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+    }
+  }
 
   // Hint: Use `globalThis` to pass variables to global teardown.
   globalThis.__TEARDOWN_MESSAGE__ = '\nTearing down...\n';
