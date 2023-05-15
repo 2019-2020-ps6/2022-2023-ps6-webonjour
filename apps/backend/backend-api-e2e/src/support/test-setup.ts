b2e85316f423fa599a4bca841df7d2f463b8952a
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-05-15 09:56:43 +0200

    fix: config
    Ref: #235

--- apps/backend/backend-api-e2e/src/support/test-setup.ts
+++ apps/backend/backend-api-e2e/src/support/test-setup.ts
@@ -3,26 +3,13 @@ import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import { config as dotenv } from 'dotenv';
-if (fs.existsSync(path.resolve('.env'))) {
-  dotenv({
-    path: path.resolve('.env'),
-    debug: true,
-  });
-} else {
-  dotenv({
-    path: path.resolve('apps/backend/backend-api-e2e/.env'),
-    debug: true,
-  });
-}
 import config from 'config';
 
 module.exports = async function () {
   // check if there is a .env file in the current directory
   // if there is, load it else load the .env file in the api directory
 
-  const port = config.get('port');
-  const host = config.get('host');
-  axios.defaults.baseURL = `http://${host}:${port}`;
+  axios.defaults.baseURL = `http://localhost:8000`;
 
   // wait for the server to start
   while (200) {
