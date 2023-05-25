7c57ec5299580c6ce6bca05eaf6ba9d92c610858
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-05-25 20:22:36 +0200

    fix: add missing module in test
    Ref: #260

--- libs/front-end/shared/common/src/lib/services/family-member/family-member.service.spec.ts
+++ libs/front-end/shared/common/src/lib/services/family-member/family-member.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FamilyMemberService } from './family-member.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('FamilyMemberService', () => {
   let service: FamilyMemberService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(FamilyMemberService);
   });
 
