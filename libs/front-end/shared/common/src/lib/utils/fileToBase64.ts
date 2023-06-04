39b462921b7e98f4396820ca624d833b04fb4cb8
Edgar B <edgar.bizel@etu.univ-cotedazur.fr>
2023-06-04 09:42:25 +0200

    fix: fileToBase64 safety measures
    Ref: #316

--- libs/front-end/shared/common/src/lib/utils/fileToBase64.ts
+++ libs/front-end/shared/common/src/lib/utils/fileToBase64.ts
@@ -1,5 +1,15 @@
 export const fileToBase64 = (file: File): Promise<string> => {
   return new Promise<string>((resolve, reject) => {
+    if (!file) {
+      reject('No file');
+    }
+    if (!(file instanceof File)) {
+      reject('Not a file');
+    }
+    if (!file.type.match('image.*')) {
+      reject('Not an image');
+    }
+
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result?.toString() as string);
