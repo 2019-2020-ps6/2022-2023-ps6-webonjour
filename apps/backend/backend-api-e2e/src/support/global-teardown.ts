d5ad9583e53fe4642fc78077afdc58996a1facbf
Ozeliurs <maxime.billy@etu.univ-cotedazur.fr>
2023-06-12 13:45:08 +0200

    feat: add native podman support
    Ref. #362

--- apps/backend/backend-api-e2e/src/support/global-teardown.ts
+++ apps/backend/backend-api-e2e/src/support/global-teardown.ts
@@ -6,7 +6,22 @@ module.exports = async function () {
   // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
   // Hint: `globalThis` is shared between setup and teardown.
   // stop the server
+  let executor = 'docker compose';
+
+  // Check if docker compose is installed
+  try {
+    subProcessSync('docker compose --version', true);
+  } catch (e) {
+    executor = 'podman-compose';
+    try {
+      subProcessSync('podman-compose --version', true);
+    } catch (e) {
+      console.log('Please install docker compose or podman compose');
+      process.exit(1);
+    }
+  }
+
   killSubProcesses('API');
-  subProcessSync('docker compose -f docker-compose.yml down', true);
+  subProcessSync(`${executor} -f docker-compose.yml down`, true);
   console.log(globalThis.__TEARDOWN_MESSAGE__);
 };
