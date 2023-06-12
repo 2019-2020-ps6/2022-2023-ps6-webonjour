d833628ac90e51c6cb8d2ee01f48de39d49f1ffd
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-06-13 00:08:04 +0200

    feat: add test for login page
    Ref: #368

--- apps/front-end/back-office-e2e/src/login/login.fixture.ts
+++ apps/front-end/back-office-e2e/src/login/login.fixture.ts
@@ -4,11 +4,15 @@ export class LoginFixture {
   readonly email: Locator;
   readonly password: Locator;
   readonly loginButton: Locator;
+  readonly invalidEmailMessage: Locator;
 
   constructor(readonly page: Page) {
     this.email = page.locator('[formcontrolname="email"]');
     this.password = page.locator('[formcontrolname="password"]');
     this.loginButton = page.locator('button:has-text("Login")');
+    this.invalidEmailMessage = page
+      .locator('form div')
+      .getByText('Email is invalid');
   }
 
   async goto() {
--- apps/front-end/back-office-e2e/src/login/login.spec.ts
+++ apps/front-end/back-office-e2e/src/login/login.spec.ts
@@ -7,4 +7,16 @@ test.describe('Login', () => {
     await fixtures.loginPage.login('email@email.com', 'password');
     await expect(fixtures.loginPage.page).toHaveURL(/dashboard/);
   });
+
+  test('should inform user about invalid email', async ({ fixtures }) => {
+    await fixtures.loginPage.goto();
+    await fixtures.loginPage.login('invalid-email', 'password');
+    await expect(fixtures.loginPage.invalidEmailMessage).toBeVisible();
+  });
+
+  test('should inform user about invalid credentials', async ({ fixtures }) => {
+    await fixtures.loginPage.goto();
+    await fixtures.loginPage.login('email@email.org', 'password');
+    await expect(fixtures.loginPage.page).toHaveURL(/login/);
+  });
 });
