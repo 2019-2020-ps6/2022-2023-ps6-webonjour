165f0c8d7210ca8975145820f88822d875377c8d
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-06-13 18:21:57 +0200

    feat: generate project
    Ref: #383

---
+++ apps/front-end/cross-office-e2e/playwright.config.ts
@@ -0,0 +1,21 @@
+import type { PlaywrightTestConfig } from '@playwright/test';
+
+import { baseConfig } from '../../../playwright.config.base';
+
+const config: PlaywrightTestConfig = {
+  ...baseConfig,
+  globalSetup: require.resolve(
+    'apps/front-end/cross-office-e2e/src/support/global-setup.ts'
+  ),
+  globalTeardown: require.resolve(
+    'apps/front-end/cross-office-e2e/src/support/global-teardown.ts'
+  ),
+  use: {
+    ...baseConfig.use,
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+};
+
+export default config;
