1f5d671d497c75e3aea48600fcb46bdf32f35c9e
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-06-13 23:02:53 +0200

    fix: this shiet
    Ref: #383

--- apps/front-end/cross-office-e2e/src/app.spec.ts
+++ apps/front-end/cross-office-e2e/src/app.spec.ts
@@ -1,32 +1,31 @@
 import { expect } from '@playwright/test';
 import { test } from '@webonjour/fixtures-e2e';
-import { environment, protocol } from '@webonjour/shared/environments';
-
-const b_test = test.extend({
-  baseURL: `${protocol(environment.back_office.secure)}://${
-    environment.back_office.domain
-  }`,
-});
-const f_test = test.extend({
-  baseURL: `${protocol(environment.front_office.secure)}://${
-    environment.front_office.domain
-  }`,
-});
 
 test.describe('cross-office', () => {
-  b_test('back should work', async ({ page }) => {
-    await page.goto('/');
+  test.afterEach(async ({ backOfficePage, frontOfficePage }) => {
+    await backOfficePage.close();
+    await frontOfficePage.close();
+  });
+
+  test('back should work', async ({ BackOffice }) => {
+    await BackOffice.loginPage.goto();
     expect(true).toBeTruthy();
   });
 
-  f_test('front should work', async ({ page }) => {
-    await page.goto('/');
+  test('front should work', async ({ FrontOffice }) => {
+    await FrontOffice.floorSelectionPage.goto();
     expect(true).toBeTruthy();
   });
 
-  f_test('floor selection should work', async ({ fixtures }) => {
-    await fixtures.FrontOffice.floorSelectionPage.goto();
-    fixtures.FrontOffice.floorSelectionPage.selectFloor(0);
+  test('floor selection should work', async ({ FrontOffice }) => {
+    await FrontOffice.floorSelectionPage.goto();
+    await FrontOffice.floorSelectionPage.selectFloor(0);
+    expect(true).toBeTruthy();
+  });
+
+  test('using both', async ({ BackOffice, FrontOffice }) => {
+    await BackOffice.loginPage.goto();
+    await FrontOffice.floorSelectionPage.goto();
     expect(true).toBeTruthy();
   });
 });
--- libs/fixtures-e2e/src/lib/fixture.ts
+++ libs/fixtures-e2e/src/lib/fixture.ts
@@ -1,4 +1,4 @@
-import { test as base } from '@playwright/test';
+import { Page, test as base } from '@playwright/test';
 import {
   backOfficeBuildFixtures,
   BackOfficeFixtures,
@@ -7,19 +7,38 @@ import {
   frontOfficeBuildFixtures,
   FrontOfficeFixtures,
 } from './front-office/fixture';
+import { environment, protocol } from '@webonjour/shared/environments';
 
 interface MergeFixtures {
   BackOffice: BackOfficeFixtures;
   FrontOffice: FrontOfficeFixtures;
+  backOfficePage: Page;
+  frontOfficePage: Page;
 }
 
-const test = base.extend<{ fixtures: MergeFixtures }>({
-  fixtures: async ({ page }, use) => {
-    await use({
-      BackOffice: backOfficeBuildFixtures(page),
-      FrontOffice: frontOfficeBuildFixtures(page),
+const test = base.extend<MergeFixtures>({
+  BackOffice: async ({ backOfficePage }, use) => {
+    await use(backOfficeBuildFixtures(backOfficePage));
+  },
+  FrontOffice: async ({ frontOfficePage }, use) => {
+    await use(frontOfficeBuildFixtures(frontOfficePage));
+  },
+  backOfficePage: async ({ browser }, use) => {
+    const page = await browser.newPage({
+      baseURL: `${protocol(environment.back_office.secure)}://${
+        environment.back_office.domain
+      }`,
     });
+    await use(page);
+  },
+  frontOfficePage: async ({ browser }, use) => {
+    const page = await browser.newPage({
+      baseURL: `${protocol(environment.front_office.secure)}://${
+        environment.front_office.domain
+      }`,
+    });
+    await use(page);
   },
 });
 
-export { test };
+export { test, backOfficeBuildFixtures, frontOfficeBuildFixtures };
