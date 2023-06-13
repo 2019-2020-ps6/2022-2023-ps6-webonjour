df9624661f7d6a1018ddf1dbede1dd6324293d30
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-06-13 17:33:55 +0200

    feat: move back-office e2e fixtures to fixtures-e2e library
    Ref: #380

---
+++ libs/fixtures-e2e/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'fixtures-e2e',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/fixtures-e2e',
+};
